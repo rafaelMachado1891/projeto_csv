b635f0c50c5c80650085b41cf6dda89e85fdd187
rafaelMachado1891 <rafadmn1@gmail.com>
2024-07-08 21:46:19 -0300

    função que faz a soma dos valores dos produtos que foram entregues

--- etl.py
+++ etl.py
@@ -5,6 +5,7 @@ import csv
 caminho_do_arquivo = "arquivo.csv"
 
 def ler_arquivo_csv(nome_do_arquivo: str) -> list[dict]:
+    # função para fazer a leitura de um arquivo csv
 
     lista = []
 
@@ -18,14 +19,30 @@ def ler_arquivo_csv(nome_do_arquivo: str) -> list[dict]:
 
 
 def filtrar_produtos_entregues(lista: list[dict]) -> list[dict]:
+
+    # função para filtrar os produtos que foram entregues
     produtos_filtrados = []
     for produtos in lista:
         if produtos.get("entregue") == "True":
          produtos_filtrados.append(produtos)
     return produtos_filtrados
 
+
+def somar_os_valores_da_lista(lista_dos_produtos_filtrados: list[dict]) -> float:
+    # função que soma todos os valores de uma lista
+    
+    valor_total_dos_produtos = 0
+    for produtos in lista_dos_produtos_filtrados:
+       valor_total_dos_produtos += float(produtos.get("price"))
+         
+    return valor_total_dos_produtos
+
+
+
 produtos_csv = ler_arquivo_csv(caminho_do_arquivo)
 produtos_entregues = filtrar_produtos_entregues(produtos_csv)
-print(produtos_entregues)
+valor_total = somar_os_valores_da_lista(produtos_entregues)
+
+print(valor_total)
 
 
